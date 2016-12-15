cde0a06bcae4b20a729023ed939bbaa34a66863d
michaeldumalag <michaeldumalag@gmail.com>
2016-12-14 17:20:19 -0800

    make the booklist become a state container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript the good" },
+    { title: "Harry Potter" },
+    { title: "The Dark tower" },
+    { title: "Eloquent Ruby" }
+  ]
+}
