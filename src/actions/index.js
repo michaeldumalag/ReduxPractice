3d9ab0667ca84b7193fbb5c23cbee73ec19212cd
michaeldumalag <michaeldumalag@gmail.com>
2016-12-18 15:55:36 -0800

    wire up action creator to return action type and payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
-export function selectbook(book) {
-  console.log('A book has been selected:', book.title);
+export function selectBook(book) {
+  // selectBook is an action creator, needs to return an action,
+  // an obj with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
